6826ea8aac2360d4a24e03ef8f214c15c7d51027
Paradoksi0 <sikeforparadoksi@gmail.com>
2025-02-10 15:29:34 +0300

    Add files via upload

---
+++ assets/js/header.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    var lastScrollTop = 0;
+    var delta = 5;
+    var headerHeight = $('#header').outerHeight();
+
+    $(window).scroll(function () {
+        if ($(window).width() < 710) { 
+            var isFlex = $('.header__list').css('display') === 'flex';
+            if (isFlex) { 
+
+            } else { 
+                var scroll = $(this).scrollTop();
+                if (Math.abs(lastScrollTop - scroll) <= delta)
+                    return;
+        
+                if (scroll > lastScrollTop && scroll > headerHeight) {
+                    
+                    
+                    $('#header').removeClass('header-down').addClass('header-up');
+                    $('#toggleBtn').removeClass('header-down').addClass('header-up');
+                } else {
+                   
+                    if (scroll + $(window).height() < $(document).height()) {
+                        $('#header').removeClass('header-up').addClass('header-down');
+                        $('#toggleBtn').removeClass('header-up').addClass('header-down');
+                    }
+                }
+                lastScrollTop = scroll;
+            }
+            
+        };
+        
+    });
+
+    if ($(window).width() > 710) {
+        $('#toggleBtn').click(function () {
+            $('.wrapper').toggleClass('active');
+            $('#header').toggleClass('active');
+            $('.header__button').toggleClass('active');
+            if ($('.header__button').hasClass('active')) {
+                // $('.header__button').html('&#10006;'); X
+            } else {
+                // $('.header__button').html('&#9776;'); 3
+            }
+        });
+    } else {
+        $('#toggleBtn').on('click', function () {
+            $('.header__list').slideToggle(500, function () {
+
+            }).css('display', 'flex');
+            $('.header__button').toggleClass('active');
+        });
+    }
+});
---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let tl = gsap.timeline();
+
+    tl.set(".welcome__title", { opacity: 0 }) // Текст скрыт в начале
+
+    .to(".title-mask", { 
+        duration: 0.2, 
+        opacity: 1,
+        x: "-100%",
+        ease: "power2.out" 
+    }) // Прямоугольник появляется
+
+    .to(".title-mask", { 
+        opacity: 1, // Прозрачность уходит в 0
+        duration: 0.3, 
+        x: "0%",
+        ease: "power2.in"
+    }, "+=0.2") // Ждём 0.2s перед исчезновением
+
+    .to(".title-mask" , {
+        opacity: 0,
+        duration: 0.5,
+        x: "0%",
+        ease: "power2.in"
+    }, "+=0.1")
+
+    .to(".welcome__title", { 
+        opacity: 1, 
+        duration: 0.2, 
+        ease: "power2.in"
+    }, "-=0.2") // Текст проявляется одновременно с исчезновением маски
+
+    .to(".welcome__disc", {
+        opacity: 1,
+        y: "0%",
+        duration: 0.3,
+        ease: "power2.out"
+    });
+});
